e3133f285917d33b10cd0daffb13e844403670af
hartley0426 <hartley0426@gmail.com>
2024-11-14 03:04:24 +0000

    Addition of random outcome text

---
+++ utilities/outcomes.py
@@ -0,0 +1,31 @@
+OUTCOMES_WORK = [
+    "You got a part-time job as a library monitor and you shushed some toddlers.\n\n",
+    "You got a part-time job as a customer service agent and you got verbally assaulted by someone.\n\n",
+    "You got a part-time job as a delivery driver and got food thrown at you.\n\n",
+    "You got a part-time job as a fitness instructor and lost your client while jogging.\n\n",
+    "You got a part-time job as a demonstrator and you dropped food while live.\n\n",
+    "You got a part-time job as a freelancer and coded a barely functional discord economy bot.\n\n",
+    "You got a part-time job as a house sitter and the house caught on fire.\n\n",
+    "You got a part-time job as a door-to-door salesman and sold nothing.\n\n",
+    "You got a part-time job as a parking attendant and you watched a pileup occur.\n\n",
+    "You got a part-time job as a barista and spilled coffee on a customer.\n\n"
+    "You got a part-time job as a dog walker and lost all the dogs.\n\n"
+    "You got a part-time job as a babysitter and the baby locked you out of the house.\n\n"
+    "You got a part-time job as a cashier and accidentally scanned your own hand.\n\n"
+    "You got a part-time job as a lifeguard and got stuck in the pool’s floaty.\n\n"
+    "You got a part-time job as a janitor and accidentally mopped yourself into a corner.\n\n"
+    "You got a part-time job as a security guard and fell asleep during a break-in.\n\n"
+    "You got a part-time job as a photographer and forgot to remove the lens cap the entire shoot.\n\n"
+    "You got a part-time job as a tutor and got outsmarted by a five-year-old.\n\n"
+    "You got a part-time job as a movie usher and spilled popcorn everywhere.\n\n"
+    "You got a part-time job as a pet groomer and ended up needing a groom yourself.\n\n"
+    "You got a part-time job as a pizza chef and burned all the pizzas.\n\n"
+    "You got a part-time job as a social media manager and posted to the wrong account.\n\n"
+    "You got a part-time job as an ice cream truck driver and ran out of ice cream on your first stop.\n\n"
+    "You got a part-time job as a bookkeeper and misplaced the ledger.\n\n"
+    "You got a part-time job as a mascot and passed out from heat exhaustion.\n\n"
+    "You got a part-time job as a museum guide and tripped into a priceless exhibit.\n\n"
+    "You got a part-time job as a window cleaner and left more streaks than you removed.\n\n"
+    "You got a part-time job as an assistant car mechanic and accidentally installed a tire backward.\n\n"
+    "You got a part-time job as a substitute teacher and lost control of the classroom in five minutes.\n\n"
+    ]
